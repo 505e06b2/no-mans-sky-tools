f769eb555c8aef9d677aa35c0dca4d6f5cdfe082
Jake <505e06b2@gmail.com>
2022-05-14 20:48:47 +0100

    Initial commit - power grid

---
+++ js/power.mjs
@@ -0,0 +1,20 @@
+"use strict";
+
+const night_time_s = 900;
+const battery_capacity_kp = 45000;
+const solar_panel_max_output_kps = 50;
+
+window.onload = async () => {
+	const panels_elem = document.querySelector('#panels');
+	const batteries_elem = document.querySelector('#batteries');
+
+	document.querySelector('#usage').oninput = (e) => {
+		let power_usage = parseInt(e.target.value);
+		if(isNaN(power_usage)) power_usage = 0;
+
+		const battery_capacity = night_time_s * power_usage;
+
+		panels_elem.innerText = Math.ceil(power_usage / solar_panel_max_output_kps * 2);
+		batteries_elem.innerText = `${Math.ceil(battery_capacity / battery_capacity_kp)} (${battery_capacity}kP)`;
+	};
+};
